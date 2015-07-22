0fdfeba0ca6db989d98e1809cb79722ae0ecbe5b
ghrud92 <ghrud92@gmail.com>
2015-07-22 16:27:47 +0900

    무엇이 문제인지 노력중

--- app/src/main/java/com/test/contacts/contactsaccess/MainActivity.java
+++ app/src/main/java/com/test/contacts/contactsaccess/MainActivity.java
@@ -1,48 +1,83 @@
 package com.test.contacts.contactsaccess;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    protected String[] names = {"abc", "def", "ghi"};
+    protected String[] numbers = {"0106745","16546","6351864"};
+    protected int[] ids = {1,2,3};
+
+    TextView txt;
+    ListView list;
+    ContentValues cv;
+    Uri u;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView)findViewById(R.id.text);
+        list = (ListView)findViewById(R.id.list);
+        Uri uri = getTheUri();
+        grantUriPermission("ContactsContract.CommonDataKinds.Phone.CONTENT_URI", uri, 2);
+        insertContacts(names, numbers, ids);
 
         Button insertBtn = (Button) findViewById(R.id.insertBtn);
         insertBtn.setOnClickListener(new View.OnClickListener() {
-                 public void onClick(View v) {
-                     Toast.makeText(getApplicationContext(), "inset Button",
-                             Toast.LENGTH_LONG).show();
-                 }
+                                        public void onClick(View v) {
+                                             Toast.makeText(getApplicationContext(), "inset Button",
+                                                     Toast.LENGTH_LONG).show();
+                                            //insertContacts(names,numbers,ids);
+                                         }
              }
         );
 
         Button modifyBtn = (Button) findViewById(R.id.modifyBtn);
         modifyBtn.setOnClickListener(new View.OnClickListener() {
-                 public void onClick(View v) {
-                     Toast.makeText(getApplicationContext(), "modify Button",
-                             Toast.LENGTH_LONG).show();
-                 }
-             }
+                                         public void onClick(View v) {
+                                             Toast.makeText(getApplicationContext(), "modify Button",
+                                                     Toast.LENGTH_LONG).show();
+                                         }
+                                     }
         );
 
         Button deleteBtn = (Button) findViewById(R.id.deleteBtn);
         deleteBtn.setOnClickListener(new View.OnClickListener() {
-                 public void onClick(View v) {
-                     Toast.makeText(getApplicationContext(), "delete Button",
-                             Toast.LENGTH_LONG).show();
-                 }
-             }
+                                         public void onClick(View v) {
+                                             Toast.makeText(getApplicationContext(), "delete Button",
+                                                     Toast.LENGTH_LONG).show();
+                                         }
+                                     }
         );
+    }
+
+    public static Uri getTheUri() {
+        return Uri.parse("content://com.android.contacts/contacts");
+    }
+
+    protected void insertContacts(String[] names, String[] numbers, int[] ids) {
+        ContentValues cv = new ContentValues();
+        //this.grantUriPermission("com.test.contacts.contactsaccess", Uri.parse("content://com.android.contacts/contacts"), MODE_WORLD_WRITEABLE);
+        this.grantUriPermission("com.example.contactsdemo", Uri.parse("content://com.android.contacts/contacts"), MODE_WORLD_WRITEABLE);
+        for(int i = 0; i<names.length; i++) {
+            cv.put(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,names[i]);
+            cv.put(ContactsContract.CommonDataKinds.Phone.NUMBER,numbers[i]);
+            cv.put(ContactsContract.CommonDataKinds.Phone._ID,ids[i]);
+            u = getContentResolver().insert(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, cv);
+        }
     }
 
 
